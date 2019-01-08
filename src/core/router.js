563fece7a11b8bec099c30292b4c2c874e21e2e2
mehechi <maxime1jacquet@gmail.com>
2019-01-08 11:33:06 +0100

    init

--- src/core/router.js
+++ src/core/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+//Import feature for route 
+import Home from '../features/home/Home.vue'
+import About from '../features/about/About.vue'
+
 
 export default new Router({
   mode: 'history',
@@ -16,10 +20,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     }
   ]
 })
---
+++ src/features/about/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/shared/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'about',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
