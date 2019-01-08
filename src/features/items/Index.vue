036eb654e8de68beef8d0e2c78243969d9a13655
Unknown <dck.romain@gmail.com>
2019-01-08 14:56:59 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/features/items/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul id="example"><!--
+  <li v-for="(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>-->
+</ul>
+</template>
+
+<script>
+import Items from '../../shared/Item';
+
+export default {
+  name: 'item',
+
+  components: {
+    Items
+  },
+
+}
+</script>
---
+++ src/shared/Item.vue
@@ -0,0 +1,23 @@
+<template>
+  <a 
+    class="item" 
+    :href="link"
+  >
+    {{title}}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+
+  props: {
+    title : {type:String, default:''},
+    link : {type:String, default:''},
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
