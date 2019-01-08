1f6d2c466d4e7bd07d4d478b3f898cd6ea3043b3
mehechi <maxime1jacquet@gmail.com>
2019-01-08 11:59:43 +0100

    feat(butoon component)

--- src/features/home/Index.vue
+++ src/features/home/Index.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
+    
+    <Button
+      :title='"ae"'
+      :link='"http://google.com"'
+    ></Button>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Button from '../../shared/Button';
 
 export default {
   name: 'home',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Button
+  },
+
 }
 </script>
---
+++ src/shared/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <a 
+    class="button" 
+    :href="link"
+  >
+    {{title}}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+
+  props: {
+    title : {type:String, default:''},
+    link : {type:String, default:''},
+  },
+}
+</script>
+
+<style lang="scss">
+.button{
+  background-color: #000;
+  color:#FFF;
+  width: auto;
+  padding:5px 20px;
+}
+</style>
