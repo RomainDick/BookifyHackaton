52bcf88e663b10bd5ade8fa65b6e205c666507ac
mehechi <maxime1jacquet@gmail.com>
2019-01-09 10:38:29 +0100

    feat(jwt_decode) add id in store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,26 +1,25 @@
 import Vue 						from 'vue';
 import * as fromTypes from './types.js';
+import jwt_decode 		from 'jwt-decode';
 
 export const actions = {
 
 	[fromTypes.LOGIN_USER]({commit}, payload){
 		
-		//for test
-		// commit(
-		// 	fromTypes.LOGIN_USER_BOLEAN,
-		// 	true
-		// );
-		//end for test
-
 		Vue.http
 		.post(
 			Vue.config.environments.baseURL+'login',
 			payload
 		)
 		.then(response => {
+			const tokenDecoded = jwt_decode(response.body.token);
+			const user = {
+				id : tokenDecoded.id,
+				token : response.body.token
+			}
 			commit(
 				fromTypes.LOGIN_USER,
-				response.body
+				user
 			);
 			commit(
 				fromTypes.LOGIN_USER_BOLEAN,
--- src/store/states.js
+++ src/store/states.js
@@ -3,7 +3,8 @@ export const states = {
   user : {
     login : false,
     infos : {
-      token : null
+      token : null,
+      id : null,
     }
   },
 
