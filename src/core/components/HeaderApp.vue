36fc2b1c34d1404ea2b2e1d47bf79d4c645da3aa
mehechi <maxime1jacquet@gmail.com>
2019-01-11 16:34:42 +0100

    fix(btn deco)

--- src/core/components/HeaderApp.vue
+++ src/core/components/HeaderApp.vue
@@ -25,7 +25,7 @@
           <router-link class='HeaderApp__link' to="/account">Mon compte</router-link>
         </li>
         <li v-if='getUserLogin'>
-          <button class='HeaderApp__link' v-on:click="decoUser()">Déconnexion</button>
+          <a class='HeaderApp__link' v-on:click="decoUser()">Déconnexion</a>
         </li>
         
         <li>
